61cd87e707b6533f5cd0ea1250713b41d0a58a91
Arko <arkotesla@gmail.com>
2025-03-14 12:53:07 +0600

    wow

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
 import LandingPage from './Landingpage'
 
 const Home = () => {
